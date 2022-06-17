3f2d4573d2928eeb251cfba3ffc205a89eefcfb9
Saif-ul Hussain <saif-ulhussain@saif-uls-air.home>
2022-06-17 11:03:32 +0100

    app.rb conflicts resolved

--- app.rb
+++ app.rb
@@ -1,28 +1,28 @@
-require "sinatra/base"
-require "sinatra/reloader"
-require "./lib/spaces"
-require "./database_connection_setup"
-require_relative "lib/user"
+require 'sinatra/base'
+require 'sinatra/reloader'
+require './lib/spaces'
+require './database_connection_setup'
+require_relative 'lib/user'
 
 class MakersBnB < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
 
-  get "/" do
+  get '/' do
     erb :index
   end
 
-  post "/user" do
+  post '/user' do
     User.create(email: params[:email], password: params[:password])
-    redirect "/user/new"
+    redirect '/user/new'
   end
 
-  get "/user/new" do
+  get '/user/new' do
     erb :'user/new'
   end
 
-  get "/spaces" do
+  get '/spaces' do
     @spaces = Spaces.all
     erb :spaces
   end
@@ -32,13 +32,13 @@ class MakersBnB < Sinatra::Base
     redirect "/spaces"
   end
 
-  get "/spaces/new" do
+  get '/spaces/new' do
     erb :'spaces/new'
   end
 
-  post "/spaces/new/create" do
+  post '/spaces/new/create' do
     Spaces.create(name: params[:name], description: params[:description], price: params[:price], available_from: params[:available_from], available_to: params[:available_to])
-    redirect "/spaces"
+    redirect '/spaces'
   end
 
   run! if app_file == $0
