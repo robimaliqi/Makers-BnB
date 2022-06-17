5b5039cf79836e51963aafb661513a8b18d4a122
Osman <osman_saeed@hotmail.co.uk>
2022-06-17 15:46:50 +0100

    html reverted

--- lib/spaces.rb
+++ lib/spaces.rb
@@ -2,34 +2,38 @@ require 'pg'
 require_relative 'database_connection'
 
 class Spaces
-  attr_reader :id, :name, :description, :price, :available
+  attr_reader :id, :name, :description, :price, :available, :available_from, :available_to
 
-  def initialize( id:, name:, description:, price:, available:)
+  def initialize( id:, name:, description:, price:, available:, available_from:, available_to:)
     @name = name
     @id = id
     @description = description
     @price = price
     @available = available
+    @available_from = available_from
+    @available_to = available_to
   end 
 
-  def self.create(name:, description:, price:)
+  def self.create(name:, description:, price:, available_from:, available_to:)
     result = DatabaseConnection.query(
-      "INSERT INTO spaces (name, description, price) VALUES($1, $2, $3) RETURNING id, name, description, price, available;",
-     [name, description, price.to_f]
+      "INSERT INTO spaces (name, description, price, available_from, available_to) VALUES($1, $2, $3, $4, $5) RETURNING id, name, description, price, available, available_from, available_to;",
+      [name, description, price.to_f, available_from, available_to]
     )
       Spaces.new(
         id: result[0]['id'],
         name: result[0]['name'],
         description: result[0]['description'],
         price: result[0]['price'],
-        available: result[0]['available']
+        available: result[0]['available'],
+        available_from: result[0]['available_from'],
+        available_to: result[0]['available_to'],
       )
   end
 
   def self.book(id:)
     spaces = DatabaseConnection.query(
       "UPDATE spaces SET available=FALSE WHERE id=$1 
-      RETURNING id, name, description, price, available;",
+      RETURNING id, name, description, price, available, available_from, available_to; ",
       [id]
     )
   end
@@ -42,7 +46,9 @@ class Spaces
         name: space['name'],
         description: space['description'],
         price: space['price'],
-        available: space['available']
+        available: space['available'],
+        available_from: Date.parse(space["available_from"]).strftime('%d/%m/%Y'),
+        available_to: Date.parse(space["available_to"]).strftime('%d/%m/%Y'),
       )
     end
   end
--- spec/Features/create_new_listing_spec.rb
+++ spec/Features/create_new_listing_spec.rb
@@ -1,25 +1,33 @@
 feature "Create listing page" do
   scenario "User can create a new listing" do
-    visit("/")
-    click_button("View Spaces")
+    register_and_login
     click_button("List a Space")
     expect(page).to have_current_path("/spaces/new")
     fill_in("name", with: "Space_1")
+    fill_in("available_from", with: "2022-01-01")
+    fill_in("available_to", with: "2022-01-07")
+    fill_in("description", with: "a place")
     click_button("List my space")
     expect(page).to have_current_path("/spaces")
     expect(page).to have_content("Space_1")
   end
 
   scenario "User can create multiple listings" do
-    visit("/")
-    click_button("View Spaces")
+    register_and_login
     click_button("List a Space")
     expect(page).to have_current_path("/spaces/new")
     fill_in("name", with: "Space_1")
+    fill_in("price", with: "100")
+    fill_in("available_from", with: "2022-01-01")
+    fill_in("available_to", with: "2022-01-07")
     click_button("List my space")
+    
+    expect(current_path).to eq '/spaces'
     click_button("List a Space")
-    expect(page).to have_current_path("/spaces/new")
     fill_in("name", with: "Space_2")
+    fill_in("price", with: "100")
+    fill_in("available_from", with: "2022-01-01")
+    fill_in("available_to", with: "2022-01-07")
     click_button("List my space")
     expect(page).to have_current_path("/spaces")
     expect(page).to have_content("Space_1")
@@ -27,25 +35,41 @@ feature "Create listing page" do
   end
 
   scenario "user can add description to listing" do
-    visit("/")
-    click_button("View Spaces")
+    register_and_login
     click_button("List a Space")
     expect(page).to have_current_path("/spaces/new")
     fill_in("name", with: "Space_1")
     fill_in("description", with: "beautiful relaxing space")
+    fill_in("available_from", with: "2022-01-01")
+    fill_in("available_to", with: "2022-01-07")
     click_button("List my space")
     expect(page).to have_content("beautiful relaxing space")
   end
 
   scenario "user can add a price to listing" do
-    visit("/")
-    click_button("View Spaces")
+    register_and_login
     click_button("List a Space")
     expect(page).to have_current_path("/spaces/new")
     fill_in("name", with: "Space_1")
     fill_in("description", with: "beautiful relaxing space")
     fill_in("price", with: 100)
+    fill_in("available_from", with: "2022-01-01")
+    fill_in("available_to", with: "2022-01-07")
     click_button("List my space")
     expect(page).to have_content("100")
+  end
+
+  scenario "user can add a range of dates" do
+    register_and_login
+    click_button("List a Space")
+    expect(page).to have_current_path("/spaces/new")
+    fill_in("name", with: "Space_1")
+    fill_in("description", with: "beautiful relaxing space")
+    fill_in("price", with: 100)
+    fill_in("available_from", with: "2022-01-01")
+    fill_in("available_to", with: "2022-01-07")
+    click_button("List my space")
+    expect(page).to have_content("01/01/2022")
+    expect(page).to have_content("07/01/2022")
   end
 end
